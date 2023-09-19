4a1b0065bc8c39bd5e445a7a8d2da36426acf013
Yogesh Goyal <yogeshgoyal1989@gmail.com>
2023-09-19 13:04:20 -0400

    Creating category post method - injecting DbContext into controller ( not  the right way ) - will use repository pattern

--- API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
+++ API/CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CodePulse.API.Data;
+using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodePulse.API.Controllers
@@ -8,5 +11,36 @@ namespace CodePulse.API.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
+        private readonly ApplicationDbContext dbContext;
+        public CategoriesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            // Map DTO to Domain model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle,
+            };
+
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+
+            // domain model to dto
+
+            var response = new CategoryDto
+            {
+                Id= category.Id,
+                Name = category.Name,
+                UrlHandle= category.UrlHandle,
+            };
+
+            return Ok(response);
+        }
     }
 }
